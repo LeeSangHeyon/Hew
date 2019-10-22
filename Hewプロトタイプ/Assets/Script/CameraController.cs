db9bb548fde8cf8f820f7b8a0d288aa11a8a0a0f
MaricoEMT <why386star@hotmail.com>
2019-10-22 23:03:13 +0900

    プロトタイプ-キャラ移動/カメラ
    10/22

---
+++ Hewプロトタイプ/Assets/Script/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Transform Player1;
+    Transform Player2;
+    new Camera camera;
+    Transform Camera0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player1 = GameObject.Find("Player1").transform;
+        Player2 = GameObject.Find("Player2").transform;
+        // メインカメラを取得
+        camera = Camera.main;
+        Camera0 = camera.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float PositionX = (Player1.position.x + Player2.position.x) / 2;
+        float PositionZ = (Player1.position.z + Player2.position.z) / 2;
+
+        float criticalX = Mathf.Abs (Player1.position.x - Player2.position.x) / 2 - 8.0f;
+        float criticalZ = Mathf.Abs (Player1.position.z - Player2.position.z) / 2 - 6.0f;
+
+        float PositionY = 24.0f + (criticalX > 0 ? criticalX : 0)
+            + (criticalZ > 0 ? criticalZ : 0);
+
+        float CameraReviseZ = -7.0f - (PositionY - 24.0f) * 0.6f;
+
+        Vector3 CameraPosition = new Vector3(PositionX, PositionY, PositionZ + CameraReviseZ);
+
+        Camera0.position = Vector3.MoveTowards (Camera0.position, CameraPosition, 16.0f * Time.deltaTime) ;
+    }
+}
